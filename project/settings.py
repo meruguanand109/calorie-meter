368fb71f61bc3439347cecd076ab3305d961a59f
Anand Merugu <120920385+meruguanand109@users.noreply.github.com>
2026-02-04 11:04:55 +0530

    Set DEBUG to False and configure ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL ='media/'
 
+DEBUG = False
+
+ALLOWED_HOSTS = ['.onrender.com']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
